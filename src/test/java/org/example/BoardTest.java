8e8dda9fb4eb92ad60bedb3d447c6dcd6afdf60f
Marco León <marco.leon11@uabc.edu.mx>
2022-11-20 13:16:04 -0800

    Pruebas Unitarias

--- src/test/java/org/example/BoardTest.java
+++ src/test/java/org/example/BoardTest.java
@@ -2,39 +2,167 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
 
     @Test
-    void drawBackground() {
+    void actionPerformed() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
 
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
+    }
+
+    @Test
+    void paintComponent() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
+    }
+
+    @Test
+    void keyTyped() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
+    }
+
+    @Test
+    void keyPressed() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
+    }
+
+    @Test
+    void keyReleased() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
+    }
+
+    @Test
+    void drawBackground() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 
     @Test
     void drawScore() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
 
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 
     @Test
     void populateTools() {
-        //check if the tools are in the right position
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
 
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 
     @Test
     void populatePlayers() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 
     @Test
     void collectTools() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 
     @Test
     void growingPlant() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 
     @Test
     void checkState() {
+        ArrayList<Player> playerList = new ArrayList<>();
+        Player player1 = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        playerList.add(player1);
+        assertEquals(player1.getScore(),"0");
     }
 }
---
+++ src/test/java/org/example/Player2Test.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.event.KeyEvent;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Player2Test {
+
+    @Test
+    void keyPressed() {
+        Player player = new Player(0, 0, "") {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+        };
+        assertEquals(player.getX(),0);
+        assertEquals(player.getY(),0);
+    }
+}
